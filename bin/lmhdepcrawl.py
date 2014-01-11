a766517aeb077f1057911af64295172ff60ff81c
Tom Wiesing <tkw01536@gmail.com>
2014-01-11 14:57:59 +0100

    extended depcrawl for importmhmodule for #47

--- bin/lmhdepcrawl.py
+++ bin/lmhdepcrawl.py
@@ -40,7 +40,16 @@ def calcDeps(dir="."):
           continue
         file = open(root+"/"+file, "r")
         for line in file:
-          m = re.search("\MathHub{([\w/]+)}", line)
+          m = re.search("\\MathHub{([\w/]+)}", line)
+          if m:
+            paths[m.group(1)] = True
+          m = re.search("\\importmhmodule\[([\w/]+)\]", line)
+          if m:
+            paths[m.group(1)] = True
+          m = re.search("\\usemhmodule\[([\w/]+)\]", line)
+          if m:
+            paths[m.group(1)] = True
+          m = re.search("\\adoptmhmodule\[([\w/]+)\]", line)
           if m:
             paths[m.group(1)] = True
 
@@ -54,7 +63,8 @@ def calcDeps(dir="."):
   toAdd = [];
   for rep in repos:
     if rep not in currentdeps:
-      toAdd.append(rep);
+      toAdd.append(rep)
+  
 
   return " ".join(toAdd);
 
