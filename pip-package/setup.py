a4109c5576a39b397a968fd0cdba9ab6c72e5e63
Tom Wiesing <tkw01536@gmail.com>
2014-07-08 16:28:33 +0200

    Windows: Fixed return code of lmh.bat

--- pip-package/setup.py
+++ pip-package/setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name='lmh',
-      version='1.7.0',
+      version='1.7.2',
       description='Local MathHub Utility (setup package)',
       author='The KWARC Group',
       author_email = "postmaster@kwarc.info",
-      scripts=['lmh'],
+      scripts=['lmh', 'lmh.bat'],
       license='GPL',
       packages=['lmh_core'],
       install_requires=['beautifulsoup4', 'psutil', 'pyapi-gitlab']
