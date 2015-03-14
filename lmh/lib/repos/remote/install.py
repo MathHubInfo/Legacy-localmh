da020482b1829731608a0b969d545ec9373b6d74
Tom Wiesing <tkw01536@gmail.com>
2015-03-14 12:50:55 +0100

    Committing work (broken)

---
+++ lmh/lib/repos/remote/install.py
@@ -0,0 +1,22 @@
+from lmh.lib.git import clone
+from lmh.lib.io import std
+from lmh.lib.repos.remote import find_source
+from lmh.lib.env import data_dir
+
+def force_install(repo):
+    """
+        Installs a repository without checking for dependencies.
+    """
+
+    std("Installing", repo)
+
+    # Find the source for the repository
+    repoURL = find_source(repo)
+
+    # If we can not find the repo
+    # then we just failed. 
+    if repoURL == False:
+        return False
+
+    # Clone the repo
+    return clone(data_dir, repoURL, repo)
