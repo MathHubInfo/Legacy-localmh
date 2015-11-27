544bb9b405e89d4c9907a84c1a21aebb787efefd
cmaeder <c.maeder@jacobs-university.de>
2015-11-27 19:07:45 +0100

    merge in hello_test

---
+++ bin/testHello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+mkdir MathHub
+cd MathHub
+mkdir HelloWorld
+cd HelloWorld
+for i in coursematerials hwexam meta-inf smglom
+do
+    git clone http://gl.mathhub.info/HelloWorld/$i.git
+done
+cd ..
+lmh pdf --depsFirst HelloWorld
+lmh omdoc --depsFirst HelloWorld
