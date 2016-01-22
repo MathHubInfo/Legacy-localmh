23bd3b1165a310f512b46c478f132fad4d5343ad
Tom Wiesing <tkw01536@gmail.com>
2016-01-22 13:44:00 +0100

    Use wget -c -N (fixes #284)

--- lmh/lib/packs/setups/MMT.py
+++ lmh/lib/packs/setups/MMT.py
@@ -25,18 +25,18 @@ class MMTPack(classes.Pack):
         return self.do_update(pack_dir,sstring)
     def do_update(self, pack_dir, sstring):
         """Install the MMT package"""
-        
+
         # Make the directory
         mkdir_p(mmt_jar_dir)
-        
+
         # and wget it, overwriting the existing jar
-        proc = subprocess.Popen([wget_executable, self.mmt_jar_source, "-O", mmt_jar_path], stderr=sys.stderr, stdout=sys.stdout)
+        proc = subprocess.Popen([wget_executable, "-c", "-N", self.mmt_jar_source, "-O", mmt_jar_path], stderr=sys.stderr, stdout=sys.stdout)
         proc.wait()
-        
+
         # chmod +x mmt_jar_path
         st = os.stat(mmt_jar_path)
         os.chmod(mmt_jar_path, st.st_mode | stat.S_IEXEC)
-        
+
         return proc.returncode == 0
 
 setup = MMTPack(get_config("setup::mmtjar::source"))
