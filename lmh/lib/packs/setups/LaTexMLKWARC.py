fbeebf96f2f3077ed661f44ceb7446456f8bb0cb
Tom Wiesing <tkw01536@gmail.com>
2016-02-15 16:51:26 +0100

    Fix capitalisation in package name (fixes #294)
    possibly forces users to rerun lmh setup --reinstall or move the folder
    manually

--- lmh/lib/packs/setups/LaTexMLKWARC.py
+++ lmh/lib/packs/setups/LaTexMLKWARC.py
@@ -4,4 +4,4 @@ from lmh.lib.config import get_config
 latexmlkwarc_source = get_config("setup::latexmlkwarc::source")
 latexmlkwarc_branch = get_config("setup::latexmlkwarc::branch")
 
-setup = classes.GitPack("LaTexMLKWARC", latexmlkwarc_source, latexmlkwarc_branch, cpanm=True)
+setup = classes.GitPack("LaTeXMLKWARC", latexmlkwarc_source, latexmlkwarc_branch, cpanm=True)
