32bc52faece459c99b7ade655be481e6acd7f49c
Tom Wiesing <tkw01536@gmail.com>
2016-02-26 10:55:14 +0100

    Bugfix for legacy commands
    ensure that the unparsed arguments are a list

--- lmh/commands/shell/run.py
+++ lmh/commands/shell/run.py
@@ -2,5 +2,5 @@ import sys
 from lmh.lib.env import run_shell
 
 def do(args, unknown):
-    code = run_shell(args.shell, args.args+unknown)
+    code = run_shell(args.shell, args.args+list(unknown))
     sys.exit(code)
